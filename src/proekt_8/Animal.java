b647e84dc66f4aa973fc33da9e99ba863e907082
SerafimovaD <daci.serafimova@gmail.com>
2023-11-28 13:45:29 +0100

    Add first java program

---
+++ src/proekt_8/Animal.java
@@ -0,0 +1,9 @@
+package proekt_8;
+
+public class Animal {
+	void bark(){
+		System.out.println("Woof-Woof");
+
+	}
+
+}
---
+++ src/proekt_8/MyClass.java
@@ -0,0 +1,11 @@
+package proekt_8;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		Animal dog=new Animal();
+		dog.bark();
+
+	}
+
+}
